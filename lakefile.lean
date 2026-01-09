8eb11b0fa4bee29425adff9c6a1826960d2e6bcc
Alexander Hicks <alexanderlhicks@gmail.com>
2026-01-09 12:02:18 +0000

    bump to 4.26

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open System Lake DSL
 
 package ExtTreeMapLemmas where version := v!"0.1.0"
 
-require "leanprover-community" / mathlib @ git "v4.22.0-rc2"
+require "leanprover-community" / mathlib @ git "v4.26.0"
 
 @[default_target]
 lean_lib ExtTreeMapLemmas
